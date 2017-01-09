2f94ad5df08d44ac3b5a31a8ccca619d161e7f2f
VieiraAkA <j14a14vieira@gmail.com>
2017-01-09 10:18:25 +0000

    Vieira

--- projecto/FXMLMenuInvestidorController.java
+++ projecto/FXMLMenuInvestidorController.java
@@ -9,14 +9,22 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import trabalhojoaotiago.Login;
@@ -27,7 +35,7 @@ import trabalhojoaotiago.Login;
  * @author Asus
  */
 public class FXMLMenuInvestidorController implements Initializable {
-    
+    Login UserLogado = Projecto.getUserLogado();
     @FXML
     private Button botao_comprar;
     @FXML
@@ -38,6 +46,12 @@ public class FXMLMenuInvestidorController implements Initializable {
     private Button BotaoSairPrograma;
     @FXML
     private Button BotaoTerminaoSessao;
+    @FXML
+    private Button Perfil;
+    @FXML
+    private Button BotaoTerminaoSessao2;
+    @FXML
+    private Button alteraDados;
     @FXML
     private Text LabelSaldoComprar ;
     @FXML
@@ -50,48 +64,70 @@ public class FXMLMenuInvestidorController implements Initializable {
     private Tab tab_vender;
     @FXML
     private Tab tab_consultar;
+    @FXML
+    private Tab tab_perfil;
+    @FXML
+    private Text dados_nome;
+    @FXML
+    private Text dados_user;
+    @FXML
+    private Text dados_telemovel;
+    @FXML
+    private Text dados_morada;
+    @FXML
+    private Text dados_email;
+    @FXML
+    private Text dados_cc;
+    @FXML
+    private Text dados_saldo;
     
+    private TabPane tabPane = new TabPane();
 
-    //jtrfhtfjycvjycgtfghcomentario
     
     @FXML
     void go_SairPrograma(ActionEvent event) {
-        Projecto.repository.Serializar();
+       Projecto.repository.Serializar();
                 //Janela.close();
         try {
-            Parent pagina = FXMLLoader.load(getClass().getResource("FXMLMenuInvestidorController.fxml"));
-            Scene janela = new Scene(pagina);
-            Stage layout = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            layout.setScene(janela);
-            layout.close();
+        Parent menuInv = FXMLLoader.load(getClass().getResource("FXMLMenuInvestidor.fxml"));
+        Scene JanelaInv = new Scene(menuInv);
+        Stage LayoutLogin = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        LayoutLogin.setScene(JanelaInv);
+        LayoutLogin.close();
+        
         } catch (IOException ex) {
-            //Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        //Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
 
     @FXML
     void go_TerminarSessao(ActionEvent event) {
-
+        
     }
 
     @FXML
     void go_to_tab_comprar(ActionEvent event) {
-
-    }
-
-    @FXML
-    void go_to_tab_consulta(ActionEvent event) {
-        LabelSaldoComprar.setText("Saldo Actual é de: ");
+        tabPane.getTabs().add(tab_comprar);
+                    
     }
 
     @FXML
     void go_to_tab_vender(ActionEvent event) {
-
+        tabPane.getTabs().add(tab_vender);
+        
     }
     
-    
-    
+    @FXML
+    void go_to_tab_consulta(ActionEvent event) {
+        tabPane.getTabs().add(tab_consultar);
+        
+    }
+     @FXML
+    void go_to_tab_Perfil(ActionEvent event) {
+        tabPane.getTabs().add(tab_perfil);
+    }
+
     
     
     /**
@@ -101,8 +137,18 @@ public class FXMLMenuInvestidorController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Login UserLogado = Projecto.getUserLogado();
+        
         LabelBemVindo.setText(" Bem Vindo " + UserLogado.getPriNome() );
+        LabelSaldoComprar.setText("Saldo Actual é de: " + UserLogado.getSaldo() + " €");
+        
+        dados_nome.setText("Nome : " + UserLogado.getPriNome() + " " + UserLogado.getUltNome());
+        dados_user.setText("Username : " + UserLogado.getUser());
+        dados_telemovel.setText("Telemovel : " + UserLogado.getTelefone());
+        dados_morada.setText("Morada : "+ UserLogado.getMorada());
+        dados_email.setText("Email : " +UserLogado.getEmail());
+        dados_cc.setText("   Cartão de Cidadão : " + UserLogado.getCartaoCidadao());
+        dados_saldo.setText("Saldo : "+ UserLogado.getSaldo());
+        
         
         
         
